fc1b9cc48f20534f0399c5ef6bc70db5f3e952ab
Valeria <lerik13@gmail.com>
2021-11-29 13:02:34 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Header } from './components/Header'
+//import './App.css';
+
+const Step1 = () => <>Step 1</>
+const Step2 = () => <>Step 2</>
+const Step3 = () => <>Step 3</>
+const Result = () => <>Result</>
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<>
+			<Header />
+			<BrowserRouter>
+				<Routes>
+					<Route exact path="/" element={<Step1/>} />
+					<Route path="/step2" element={<Step2/>} />
+					<Route path="/step3" element={<Step3/>} />
+					<Route path="/result" element={<Result/>} />
+				</Routes>
+			</BrowserRouter>
+		</>
+	);
 }
 
 export default App;
---
+++ src/Step1.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Typography from 'material-ui/styles/typography';
+import { MainContainer } from './components/MainContainer';
+
+export const Step1 = () => {
+	return <MainContainer>
+		<Typography component="h2" variant="h5">
+			Step 1
+		</Typography>
+		
+	</MainContainer>
+}
